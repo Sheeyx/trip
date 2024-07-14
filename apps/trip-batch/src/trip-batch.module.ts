807a02360b738271044e5c4dde8f5fe4223ea098
Sheeyx <sheeyx00@gmail.com>
2024-07-14 09:38:06 +0900

    feat: configure env variables

--- apps/trip-batch/src/trip-batch.module.ts
+++ apps/trip-batch/src/trip-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TripBatchController } from './trip-batch.controller';
 import { TripBatchService } from './trip-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [TripBatchController],
   providers: [TripBatchService],
 })
