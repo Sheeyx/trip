e20378708bbd685cc9f91270b51f48f8b18ab13b
Sheeyx <sheeyx00@gmail.com>
2024-07-15 11:28:54 +0500

    feat: develop single and multi image uploader mutation GraphQL APIs

--- apps/trip-api/src/libs/config.ts
+++ apps/trip-api/src/libs/config.ts
@@ -6,3 +6,13 @@ export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'm
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
+
+// IMAGE CONFIGURATION
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+}; 
