ae42a05c47e5238d225e3eeefe6597b9d1582d59
Sheeyx <sheeyx00@gmail.com>
2024-07-16 11:50:51 +0500

    feat: develop likeTargetMember GraphQL API

--- apps/trip-api/src/components/member/member.module.ts
+++ apps/trip-api/src/components/member/member.module.ts
@@ -5,9 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
+import { LikeModule } from '../like/like.module';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule],	providers: [MemberResolver, MemberService],
+	imports: [MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]), AuthModule, ViewModule,LikeModule],	providers: [MemberResolver, MemberService],
 	exports: [MemberService]
 })
 export class MemberModule {}
