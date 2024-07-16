7a8ce3a1039a23683f5a17e81aa5e9eb618569ee
Sheeyx <sheeyx00@gmail.com>
2024-07-16 11:59:04 +0500

    feat: create follow related resolver and service model

--- apps/trip-api/src/components/follow/follow.module.ts
+++ apps/trip-api/src/components/follow/follow.module.ts
@@ -1,4 +1,25 @@
 import { Module } from '@nestjs/common';
+import { FollowService } from './follow.service';
+import { FollowResolver } from './follow.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import FollowSchema from '../../schemas/Follow.model';
+import { MemberModule } from '../member/member.module';
 
-@Module({})
-export class FollowModule {}
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { 
+        name: 'Follow', 
+        schema: FollowSchema 
+      }
+    ]), 
+    AuthModule,
+    MemberModule
+  ],
+  providers: [FollowService, FollowResolver],
+  exports: [FollowService]
+})
+export class FollowModule {
+
+}
