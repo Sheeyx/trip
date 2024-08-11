61a99ec4e867b266bddb7685cd9e0b57d4fee824
Sheeyx <sheeyx00@gmail.com>
2024-08-11 17:41:02 +0900

    fix: develop job scheduler business logic

--- apps/trip-batch/src/batch.controller.ts
+++ apps/trip-batch/src/batch.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
 import { Cron, Interval, Timeout } from '@nestjs/schedule';
-import { BATCH_ROLLBACK } from './libs/config';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './libs/config';
 
 @Controller()
 export class BatchController {
@@ -13,7 +13,7 @@ export class BatchController {
     this.logger.debug("BATCH SERVER READY!")
   }
 
-  @Cron("00 * * * * *", {name: BATCH_ROLLBACK})
+  @Cron("00 00 01 * * *", {name: BATCH_ROLLBACK})
   public async batchRollback() {
     try {
       this.logger["context"] = BATCH_ROLLBACK;
@@ -24,23 +24,23 @@ export class BatchController {
     }
   }
 
-  @Cron("20 * * * * *", {name: "CRON_TEST"})
-  public async batchProperties() {
+  @Cron("20 00 01 * * *", {name: BATCH_TOP_PROPERTIES})
+  public async batchTopProperties() {
     try {
-      this.logger["context"] = "CRON_TEST";
+      this.logger["context"] = BATCH_TOP_PROPERTIES;
       this.logger.debug("EXECUTED");
-      await this.batchService.batchProperties();
+      await this.batchService.batchTopProperties();
     } catch (err) {
       this.logger.error(err)
     }
   }
 
-  @Cron("40 * * * * *", {name: "CRON_TEST"})
-  public async batchAgents () {
+  @Cron("40 00 01 * * *", {name: BATCH_TOP_AGENTS})
+  public async batchTopAgents () {
     try {
-      this.logger["context"] = "CRON_TEST";
+      this.logger["context"] = BATCH_TOP_AGENTS;
       this.logger.debug("EXECUTED");
-      await this.batchService.batchAgents();
+      await this.batchService.batchTopAgents();
     } catch (err) {
       this.logger.error(err)
     } 
