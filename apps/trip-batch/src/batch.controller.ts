30868cbbf13c628423de36c464bdc20fa3d59194
Sheeyx <sheeyx00@gmail.com>
2024-08-11 17:39:44 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers logic

--- apps/trip-batch/src/batch.controller.ts
+++ apps/trip-batch/src/batch.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK } from './libs/config';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly BatchService: BatchService) {}
+  constructor(private readonly batchService: BatchService) {}
+  private logger: Logger = new Logger("BatchController");
+
+  @Timeout(1000)
+  handleTimeout() {
+    this.logger.debug("BATCH SERVER READY!")
+  }
+
+  @Cron("00 * * * * *", {name: BATCH_ROLLBACK})
+  public async batchRollback() {
+    try {
+      this.logger["context"] = BATCH_ROLLBACK;
+      this.logger.debug("EXECUTED");
+      await this.batchService.batchRollback();
+    } catch (err) {
+      this.logger.error(err)
+    }
+  }
+
+  @Cron("20 * * * * *", {name: "CRON_TEST"})
+  public async batchProperties() {
+    try {
+      this.logger["context"] = "CRON_TEST";
+      this.logger.debug("EXECUTED");
+      await this.batchService.batchProperties();
+    } catch (err) {
+      this.logger.error(err)
+    }
+  }
+
+  @Cron("40 * * * * *", {name: "CRON_TEST"})
+  public async batchAgents () {
+    try {
+      this.logger["context"] = "CRON_TEST";
+      this.logger.debug("EXECUTED");
+      await this.batchService.batchAgents();
+    } catch (err) {
+      this.logger.error(err)
+    } 
+  }
+
+  /*
+  @Interval(1000)
+  handleInterval() {
+    this.logger.debug("INTERVAL TEST")
+  }
+  */
+
 
   @Get()
   getHello(): string {
-    return this.BatchService.getHello();
+    return this.batchService.getHello();
   }
 }
